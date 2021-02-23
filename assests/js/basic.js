74b370c840908311d5945ced55fdd80735e3d910
Ahmed Abdullahi <abdullahiahmad22@gmail.com>
2021-02-23 07:57:10 +0300

    first commit

---
+++ assests/js/basic.js
@@ -0,0 +1,14 @@
+// const slides = document.querySelectorAll('.slide');
+
+// const currentSlide = 0;
+
+// setInterval(nextSlide, 2000);
+
+// function nextSlide() {
+//     slides[currentSlide].classList.toggle('active');
+//     if (currentSlide < 4)  currentSlide++;
+//     else currentSlide = 0;
+//     slides[currentSlide].classList.toggle('active');
+
+// }
+
